c786ef77131df514864abefa84bd78bcf224fdeb
Bogdan Nechyporenko <bnechyporenko@bol.com>
2025-03-11 19:32:07 +0100

    wip

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -32,8 +32,6 @@ const authResult = async (): Promise<NextAuthResult> => {
 						password: string;
 					};
 
-					console.log(`email = ${email}`);
-
 					const c = getRequestContext();
 					const adapter = new PrismaD1(c.env.DB);
 					const prisma = new PrismaClient({ adapter });
@@ -43,8 +41,6 @@ const authResult = async (): Promise<NextAuthResult> => {
 							email,
 						},
 					});
-
-					console.log(`user = ${JSON.stringify(user)}`);
 
 					if (user) {
 						return {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,6 @@ export default async function Home({
 }) {
 	const session = await auth();
 
-	console.log(`searchParams=${JSON.stringify(searchParams)}`);
-
 	return (
 		<main className="flex items-center justify-center min-h-screen bg-background">
 			<Card className="w-full max-w-md">
@@ -73,9 +71,7 @@ export default async function Home({
 										email: formData.get("email") as string,
 										redirect: false,
 									});
-									console.log("before redirect");
 								} catch (_err) {
-									console.log("ERROR is =", _err);
 									redirectPath = "/?error=incorrect-login";
 								} finally {
 									redirect(redirectPath);
